b81095bf3035d666b3543ec4e7c0ada1370eeaa8
Muhammadqodir0903 <madinaziemova@gmail.com>
2024-02-03 00:08:50 +0500

    mmmmm

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div className="container">
+          <div className="wrapper">
+            <ul className='wrapper__list'>
+              <li className="wrapper__item">
+                <h1>imr</h1>
+              </li>
+              <li className="wrapper__item">
+                <a href="#">Home</a>
+              </li>
+              <li className="wrapper__item">
+                <a href="#">About</a>
+              </li>
+              <li className="wrapper__item">
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
+      <section>
+       
+       <img className='img' src="https://upload.wikimedia.org/wikipedia/commons/0/00/Timur_reconstruction03.jpg" alt="" />
+       <center><h1>Amir Temur haqida</h1></center>
+       <center><h3>(1336-yil – 1405-yil)</h3></center>
+ <center>
+ <p>Amir Temur (Eski turkcha: امیر تیمور – Amir Temur) (1336-yil 8-aprel – 1405-yil 18-fevral) – oʻrta asrlarning buyuk turk askari va qoʻmondoni, Temuriylar imperiyasi asoschisi, Oltin Oʻrda, Mamluq sultonligi, Dehli sultonligi, Usmonli davlati, Kavkaz va Erondagi davlatlarni barisini yengishi ortidan musulmon olamining mutloq yetakchisi, yero'lchami 4,4 mln km² ga yetishgan 14-yuzyilliknig eng kuchli davlati quruvchisi, 35 yillik boshqaruvi va yurishlari chogʻida biror urushni yutqazmagan qoʻshinboshi, tarixda koʻrilgan uchta buyuk harbiy-taktik lashkarboshilardan biri, Temurlilar uygʻonish davri yaratuvchisi, ilm-fan, meʼmorchilik, sanʼat va adabiyot homiysi.
+  <br />
+  <hr />
+
+Oilasi, shaxsiyati va kelib chiqishi
+Temurbek shabon oyining yigirma beshida, hijriy yetti yuz oʻttiz oltinchi yilda (8.04.1336), oʻn ikki hayvonli Turk yilligiga koʻra Sichqon yilida Kesh shahri yaqinidagi Xoʻja Ilgʻor qishlogʻida tulilgan. Ayricha amerikalik tarixchi Beatrice Forbes Manz xonim Temurbekning tugʻilgan yilini oʻn toʻrtinchi yuzyillikning yigirmanchi yillariga toʻgʻri kelishini kabi o'z ikkilanishli qarashlarini oldinga surib keladi va bu shubhalanishiga sabab qilib Temurbekning 1336-yilda tugʻilganligi toʻgʻrisidagi yozuvlar uning davridan keyin yozilganini va uning davriga tegishli bir nechta manbalardagi voqealarda Temurbekning yoshi uning 1320-yillar oxirida tugʻilganini koʻrsatishini aytadi (qarang, Tamerlane and the Symbolism of Sovereignty, B. F. Manz). Biroq birorta tarix kitoblarida Temurbekning tugʻilgan yili 1336-yil emas, balki boshqa yil ekanligi toʻgʻrisida bilgi umuman uchramaydi.
+<br />
+  <hr />
+
+Otasi Amir Taragʻay (oʻrta chogʻ turkchasida Taragʻay – qoraquyruq qushi) barlos qabilasining beklaridan biri va qabilaning asosiy 5 ta urugʻidan birining yetakchisi edi (qarang, The rise and rule of Tamerlane, B. F. Manz va The Mongol Period B. Spuler). Taragʻaybek Chigʻatoy ulusining taniqli beklaridan biri boʻlib, Kesh viloyatida amirlik qilgan. Amir Taragʻay yillik Ili daryosi boʻyida Xon tomonidan chaqiriladigan yurt beklarining qurultoyiga chaqirilgan va bunday yigʻinlarda qoldirmasdan qatnashgan. Butga topinuvchi oʻlaroq tugʻilgan Taragʻaybek Chigʻatoy ulusi orasida birinchilardan boʻlib islom dinini qabul qilgan. Otasi Barqul Noyondek Tarag'aybek harb va siyosat ishlariga qiziqmagan va asosan islom dinini oʻrganish bilan umrini kechirgan. Temurbek tez-tez qabrini ziyorat qiladigan va keyinchalik qabri ustiga maqbara (1373–1374) qurdirgan Shamsiddin Kulol Taragʻaybekning piri boʻlgan.
+<br />
+  <hr />
+
+Onasi Takina Xotun va yoki Takina Mohbegim toʻgʻrisida bilgilar oz. Zafarnoma (Yazdli Sharafiddin)da yolg'iz otigina soʻzlanadi. 1403-yilda Temurbek Jenoa, Fransiya, Angliya qirollariga elchi qilib yuborgan, Sultoniya bosh rohibi (archiepiscopus) Johannes de Galonifontibus Takina Xotunning oddiy oiladan ekanini yozadi. Temurli saroyida yozilgan, biroq yozuvchisi aniq bilinmagan Muyizz al-Ansab (Izzatli nasablar) asarida Takina Xotun barlos qabilasi bilan chegaradosh boʻlgan Chigʻatoy ulusining Yasaur qabilasidan ekanligi yoziladi. Temurbek bilan yuzma-yuz soʻzlashgan Ibn Haldun (1400–01) Temurbek onasini afsonaviy „Avesto“ bahodiri Manuchehrning avlodidan ekanligini aytganini yozadi. Temurbekning siyosiy kitobyozar raqibi Ahmad ibn Arabshoh oʻz Ajoyib ul-maqdur fi navaib (fi axbor) at-Taymur (arabcha: عجائب المقدور في نوائب  تيمور – oʻzbekcha: Temurdagi taqdirning ajoyibotlar) asarida uning Chingizxonning naslidan ekanligini yozadi. 18-yuzyillikda yuzaga chiqqan Temurbek kimligi toʻgʻrisidagi kitoblarda Takina Xotun Buxoro „Sadr ash-Sharia“ (Shariat qonunlarini izohlovchi)si Ubaydullo Mahbubiyning qizi oʻlaroq tanitiladi. Oʻzbek tarixchisi Turgʻun Fayziyez oʻzining „Temuriylar shajarasi“ (Toshkent, 1995) kitobida Temurbekning onasi toʻgʻrisida „Jangnomayi Amir Temur Koʻragon“ kitobiga tayangan holda Buxoro ulamolarining eng ulugʻi „Sadr ash-Sharia“ning qizi ekanligini bildiradi.
+<br />
+  <hr />
+
+Temurbekning opasi Qutlugʻ Turkon ogʻo va singlisi Shirinbeka ogʻo edi. Ular Temurbekdan oldin oʻlishgan va Samarqanddagi Shohi Zinda majmuasidagi maqbaralarda koʻmilishgan.
+<br />
+  <hr />
+
+Arabcha sohib (صاحب) va qiron (yaqinlik, yaqinlashma) soʻzlaridan tuzilib forscha sohib-i qiron (صاحب كران) holiga kelgan Sohibqiron soʻzi yulduzbilimchilari tomonidan Mushtariy (Yupiter) yulduzi va Zuhro (Venera) yulduzining bir burjda toʻplanganligi asosida tugʻilgan kishi maʼnosida qoʻllanilgan. Bu sifat baxtli, qutli va doimo gʻolib va jahonolar kishilarni ifoda etish uchun ishlatilgan. Amir Temurgada bu sifat Sohibqiron va yoki hazrat Sohibqiron shakllarida qoʻllanilgan. Temurbek 1362-yilda Seyistonga birinchi kelishi chogʻida oʻng oyogʻidan va oʻng qoʻlidan Mamaqtuv (Yazdli Sharafiddin, Zafarnoma 19a,19b, 89b) tomonidan yoydan otilgan oʻqdan yaralangan. Ayricha Temurbekning siyosiy raqiblari tomonidan unga soʻkinish maʼnosidagi Temur-i lang (forscha: تیمور لنگ) nomi qoʻllanilgan. Oqsoq Temur va yoki choʻloq Temur maʼnosida kelgan Temurlang soʻzi kunbotar (gʻarb) ellari tomonidan Tamerlane, (inglizcha), Tamburlaine (inglizcha), Tamerlan (fransuzcha), Tamorlan (ispancha), Tamerlanis (lotincha), Timurlenk (usmonli turkcha) va Temur Aksak,Tamerlan (ruscha) kabi shakllarda oʻzlashtirilgan. 1370-yilda Chingizlilar xonadonidan boʻlgan Qozonxonning qizi Saroy Mulk Xonim (Bibi Xonim)ga uylanishi ortidan Temurbek Temur Koʻragon (تیمور کورگن) va yoki Temur Guragon otini oladi. Koʻragon (xүrgen) moʻgʻulcha kuyov soʻzi boʻlib „Xonning kuyovi“ maʼnosida qoʻllanilgan. Chingizxonning qonidan boʻlmaganligi uchun Xonlik otini olmaydi va musulmonlarning boshi oʻlaroq Amir otini oladi. Temurbek Islom dini bilimlariga tayanib yurishlar qiladi va oʻzini „Islom qilichi“ sifatida taniltiradi. Islom dinini yoyish yoʻlida qilgan yurishlaridan keyin Abu Fotih otini oladi. 1402-yilda Izmir qal’a qamalida katolik Hospitaller (lotin, Ordo Fratrum Hospitalis)larini, Gurjiston(Gruziya) nasroniylarini bir necha bor yutishi ortidan Abu Gʻoziy otini oladi. Turklar oʻz odatiga koʻra oʻz yetakchilarini Bek deb atashardi. Shu yuzdan Temurbek ham Bek otini oladi. 1404-yilda Samarqandda boʻlgan Kastiliya (ispaniya) elchisi Rui Gonzales de Klavixo oʻzining sayohat qoʻlyozma kundaligida ham Temurbek otini qoʻllaydi.
+<br />
+  <hr />
+
+Temurbekning turk yoki moʻgʻul ekanligi toʻgʻrisadagi bilimlar yolgʻiz kunbotar olamidagina tortishmali. Rui Gonsales de Klavixo Temurbekning kelib chiqishini Chigʻatoy ulusining ellariga bogʻlasa, nabirasi Muhammad Taragʻay (Ulugʻbek nomi bilan tanilgan) „Tarix-i arba ulus“ kitobida Shajarat al-atrak (Turklar kelib chiqishi)ni koʻrsatadi va unga koʻra Temurbek va ota-bobolari Yofas oʻgʻli Turkning bolalari ekanligini aytadi. Turk otli kishi esa butun turklarning otasi ekanligi koʻrsatiladi. Kunbotar ellarining oʻtmish yozuvchilari oʻrtasida Temurbekni turk-moʻgʻul yoki turklashgan moʻgʻul kelib chiqishli ekanligi keng yoyilgan. Biroq Richard Bulliet (A Global history) Temurbekning qabilasi Barloslarning moʻgʻullarga aloqasi yoʻqligini soʻzlasa, Rene Grousset (qarang, L’empire des steppes, Atilla, Gengis-Khan, Tamerlan,1939 Paris) Temurbekning kelib chiqishi Chingizxonga bogʻlanganligini yolgʻiz oʻz chogʻida yozilgan kitoblardagina ekanini, biroq uni moʻgʻullik bilan umuman birligi yoʻqligini va Turk ekanligini yozadi. Temurbek tarixi boʻyicha 3 salmoqli kitobdan birisi sanalgan Ahmad ibn Arabshohning „Ajoyib ul-maqdur fi navaib at-Taymur“ kitobida boʻlsa Temurbekni barlos turki ekanligini yoziladi:
+<br />
+  <hr />
+
+„Turklarda ham Arablarda boʻlganidek qabilalar va urug'lar bordir va har bir qabilada vazir (qabilaboshi) bordir. U oʻz qabilasi ichida yashash tarzi boʻyicha maslahat beradigan yorituvchi chiroqdir. Ulardan birinchisi Arlot, ikkinchisi Jaloyir, uchinchisi Qavchin, toʻrtinchisi Barlosdir. Temur(bek) toʻrtichisidan chiqqandir.“ (4-bet).
+<br />
+  <hr />
+
+Temurbek Turk ekanligidan gʻururlanar, oʻz oʻrnida oʻziga qarata aytilgan Moʻgʻul soʻzini yomon koʻrardi (Yusuf Husayn Xon, Two studies in early mongol history, 1976). Oʻzbek tarix yozuvchilari Temurbekning soʻzsiz turk boʻlganligini aytib keladi. Temurbekning oʻzi yozganligi aytiladigan Mulfazot-i Temuriy (Temur qissalari yoki Temur tuzuklari) kitobida Taragʻaybek oʻgʻli bilan soʻzlashganda (Zuhriddin Bobokalonov, 2004, 48-bet) turk kelib chiqishli ekanligini aytgani yoziladi.</p>
+<marquee behavior="" direction="">Muallif: <i>Inomov</i></marquee>
+ </center>
+      </section>
+    </div>
+  );
+}
+
+export default App;
